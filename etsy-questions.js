757cd97fe002ed113c24eaea1a5384fb70228391
William J Weekes Jr <WilliamWeekes@Williams-MacBook-Pro.local>
2017-03-28 15:49:03 -0400

    adding files to git

---
+++ etsy-questions.js
@@ -0,0 +1,39 @@
+//Calculate the average price of all items.
+
+console.log(items);
+
+let price = items.map(price);
+
+//function priceMean() {
+  //return.length
+//}
+
+//function findingTheMean() {
+//  let total = 0
+  //let prices = []
+  //price.map =>
+
+//}
+
+
+//Get an array of items that cost between $14.00 and $18.00 USD
+
+
+
+
+//Which item has a "GBP" currency code? Display its name and price.
+
+
+
+
+//Display a list of all items which are made of wood.
+
+
+
+
+//Which items are made of eight or more materials? Display the name, number of items and the items it is made of.
+
+
+
+
+//How many items were made by their sellers?
