56256377226906aa567a1930a40864c878553385
William J Weekes Jr <WilliamWeekes@Williams-MacBook-Pro.local>
2017-03-28 23:35:23 -0400

    able to calculate the average, still working out the other questions

--- etsy-questions.js
+++ etsy-questions.js
@@ -1,37 +1,46 @@
 //Calculate the average price of all items.
 
-console.log(items);
+console.log(data);
 
-let price = items.map(price);
+
 
 //function priceMean() {
   //return.length
 //}
 
-//function findingTheMean() {
-//  let total = 0
-  //let prices = []
-  //price.map =>
+function findingTheMean() {
 
-//}
-
+  //let pricesArray = data.map(function(items){return items.price;});
+let pricesArray = data.map(items => items.price);
+  console.log(pricesArray);
+  var sum = pricesArray.reduce(function(acc, val) {
+    return acc + val;
+  });
+  console.log(sum)
+  let average = sum / pricesArray.length;
+  console.log(average.toFixed(2))
+}
+findingTheMean();
 
 //Get an array of items that cost between $14.00 and $18.00 USD
+//let teens = data.filter(object => object.age > 12 && object.age < 20); .map
 
-
+//Look at .price that == $14.00 and $18.00
 
 
 //Which item has a "GBP" currency code? Display its name and price.
 
-
+//I am gonna want to look at .currency_code
 
 
 //Display a list of all items which are made of wood.
 
-
+//Look at .materials I will need to figure the Array.prototype.indexOf() of the array.
 
 
 //Which items are made of eight or more materials? Display the name, number of items and the items it is made of.
+
+//Look at .material that length = [9]
 
 
 
